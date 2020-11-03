0c7a86c73700164003edeb503612dd06721e7c27
Mansour Malloum <mansourmalloum@gmail.com>
2020-11-03 11:38:53 +0100

    Last Update::

--- src/main/java/com/easyschools/restservices/controllers/UserController.java
+++ src/main/java/com/easyschools/restservices/controllers/UserController.java
@@ -3,7 +3,6 @@ package com.easyschools.restservices.controllers;
 
 import com.easyschools.restservices.entities.User;
 import com.easyschools.restservices.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
